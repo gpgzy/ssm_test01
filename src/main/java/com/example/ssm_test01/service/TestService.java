7f3902a9cd1c93ca17b8a2ff7c9cee31b8572247
gzy <786133379@qq.com>
2020-02-28 18:03:23 +0800

    spring boot+mybstis的一个未完成的项目 我写了一个BUG哈哈 BUG已经改了

--- src/main/java/com/example/ssm_test01/service/TestService.java
+++ src/main/java/com/example/ssm_test01/service/TestService.java
@@ -2,7 +2,7 @@ package com.example.ssm_test01.service;
 
 public class TestService {
     public void bug_method(){
-        int x;
-        int y=10/0;
+        int x=10;
+        int y=10/x;
     }
 }
